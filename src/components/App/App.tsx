54375de63538eb3a720ab4bc064cfe07a687f248
themd <themd@mail.ru>
2024-12-16 17:19:28 +0500

    add app and header

---
+++ src/components/App/App.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import AppHeader from '../AppHeader/AppHeader'
+import BurgerIngredients from "../BurgerIngredients/BurgerIngredients.tsx";
+import BurgerConstructor from "../BurgerConstructor/BurgerConstructor.tsx";
+
+function App() {
+  const [state, setState] = React.useState({
+    data: null,
+    isLoading: false,
+    hasError: false,
+  })
+  const URL = 'https://norma.nomoreparties.space'
+
+  const getProductData = () => {
+    setState({ ...state, hasError: false, isLoading: true });
+    fetch(`${URL}/api/ingredients`)
+      .then(res => res.json())
+      .then(({ data }) => setState({ ...state, data, isLoading: false }))
+      .catch(() => {
+        setState({ ...state, hasError: true, isLoading: false });
+      });
+  };
+
+  React.useEffect(() => {
+    getProductData();
+  }, []);
+
+  return (
+    <div className="wrapper text text_type_main-default">
+      <AppHeader/>
+      <main className="container d-flex">
+        {state.isLoading && 'Загрузка ...'}
+        {state.data &&
+            <>
+                <BurgerIngredients ingredients={state.data} />
+                <BurgerConstructor ingredients={state.data} />
+            </>
+        }
+      </main>
+    </div>
+)
+}
+
+export default App
---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,26 @@
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import styles from './AppHeader.module.css';
+
+function AppHeader() {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={`container ${styles.wrapperHeader}`}>
+          <nav className={styles.navLeft}>
+            <a className={styles.headerLink} href="#"><BurgerIcon className="mr-2" type="primary"/>Конструктор</a>
+            <a className={`ml-2 ${styles.headerLink}`} href="#"><ListIcon className="mr-2" type="primary"/>Лента заказов</a>
+          </nav>
+          <a className={styles.logo} href="/">
+            <Logo/>
+          </a>
+          <nav className={styles.navRight}>
+            <a className={styles.headerLink} href="#"><ProfileIcon className="mr-2" type="primary"/>Личный кабинет</a>
+          </nav>
+        </div>
+      </header>
+    </>
+  )
+
+}
+
+export default AppHeader
